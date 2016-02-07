81d9275c84f2c14af401b89daf15709273800a2b
Juho Leinonen <juholei@gmail.com>
2016-02-07 19:25:17 +0200

    Change :use to require and refer. Make things credential structures private in my-twitter.

--- src/twitterbot/my_twitter.clj
+++ src/twitterbot/my_twitter.clj
@@ -1,19 +1,19 @@
 (ns twitterbot.my-twitter
-    (:use
-        [twitter.oauth]
-        [twitter.api.restful])
-    (:require [clojure.edn :as edn]
+    (:require [twitter.oauth :refer [make-oauth-creds]]
+              [twitter.api.restful :refer [statuses-update direct-messages direct-messages-new]]
+              [clojure.edn :as edn]
               [clojure.string :as string]))
 
 (defn- load-credentials [filename]
   (edn/read-string (slurp filename)))
 
-(def credentials-from-file (load-credentials "credentials.edn"))
+(def ^:private credentials-from-file (load-credentials "credentials.edn"))
 
-(def oauth-credentials (make-oauth-creds (credentials-from-file :consumerkey)
-                                         (credentials-from-file :consumersecret)
-                                         (credentials-from-file :usertoken)
-                                         (credentials-from-file :usersecret)))
+(def ^:private oauth-credentials
+  (make-oauth-creds (credentials-from-file :consumerkey)
+                    (credentials-from-file :consumersecret)
+                    (credentials-from-file :usertoken)
+                    (credentials-from-file :usersecret)))
 
 (defn tweet [title url]
   (let [to-be-tweeted (string/join " " [title url])]
